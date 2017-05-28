3cd68cc2a847fad4782a5a323ef96c0797d840e0
matthieu <matthieu@treussart.com>
2017-05-28 10:33:01 +0200

    first commit

---
+++ alert_splunk.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+# coding: utf8
+import json
+import ast
+import os
+import sys
+import subprocess
+import smtplib
+import argparse
+from email.mime.text import MIMEText
+from datetime import datetime, timedelta
+
+
+def send_email(body, nbr):
+    msg = MIMEText("<html><head></head><body>" + str(body) + "</body></html>", 'html')
+    from_address = "alert@treussart.com"
+    to_address = "matthieu@treussart.com"
+    msg['Subject'] = '%s Alerts from SIEM' % nbr
+    msg['From'] = from_address
+    msg['To'] = to_address
+    # Send the message via our own SMTP server.
+    s = smtplib.SMTP('localhost')
+    s.send_message(msg)
+    s.quit()
+
+
+def get_alerts(query, hours):
+    now = datetime.now()
+    earliest_time = now - timedelta(hours=hours)
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    shell = "python " + current_path + "/splunk-sdk-python/search.py " \
+            "--output_mode=json --earliest_time=" + earliest_time.isoformat() + \
+            " 'search " + query + " '"
+    return json.loads(subprocess.getoutput(shell))
+
+
+def alert_splunk(time_period, severity):
+    """Main function."""
+    suricata_query = "index=suricata event_type=alert"
+    ossec_query = "index=ossec"
+    list_alerts = ""
+    nbr = 0
+    alerts_suricata = get_alerts(suricata_query, time_period)
+    for alert in alerts_suricata['results']:
+        alert_raw = ast.literal_eval(alert['_raw'])
+        if int(alert_raw['alert']['severity']) >= severity:
+            nbr += 1
+            list_alerts = list_alerts + "<h4>" + str(alert_raw['alert']['signature']) + \
+                          "</h4>" + "<pre>" + alert['_raw'] + "</pre><br/>"
+    alerts_ossec = get_alerts(ossec_query, time_period)
+    for alert in alerts_ossec['results']:
+        alert_raw = ast.literal_eval(alert['_raw'])
+        if int(alert_raw['rule']['level']) >= severity:
+            nbr += 1
+            list_alerts = list_alerts + "<h4>" + str(alert_raw['rule']['comment']) + \
+                          "</h4>" + "<pre>" + alert['_raw'] + "</pre><br/>"
+    if list_alerts:
+        send_email(list_alerts, nbr)
+
+
+if __name__ == "__main__":
+    sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/splunk-sdk-python")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--time_period",
+                        help="the elapsed time to check",
+                        type=int,
+                        default=2)
+    parser.add_argument("--severity",
+                        help="the level severity to consider",
+                        type=int,
+                        default=6)
+    args = parser.parse_args()
+    time_period = args.time_period
+    severity = args.severity
+    alert_splunk(time_period, severity)
